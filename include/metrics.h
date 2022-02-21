809d4825eb87f3c4bb366c6872aab179a62d092d
Patrick Roche <pmr25@pitt.edu>
2022-02-21 12:28:56 -0500

    add methods

--- include/metrics.h
+++ include/metrics.h
@@ -1,8 +1,21 @@
 #ifndef METRICS_H
 #define METRICS_H
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdint.h>
+
 int dpi_gather_metrics(void);
 double dpi_get_monitor_scale(void);
+uint32_t dpi_get_resolution_x(void);
+uint32_t dpi_get_resolution_y(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* METRICS_H */
 
--- src/metrics.c
+++ src/metrics.c
@@ -1,3 +1,5 @@
+#include "metrics.h"
+
 #if defined _WIN32
 
 #include <WinUser.h>
@@ -10,15 +12,60 @@
 
 #endif
 
-static double scale = 1.0;
+#define REFERENCE_PIXEL_HEIGHT 1080.0
 
-void dpi_gather_metrics(void)
+static double scale = 1.0;
+static uint32_t resolution_x;
+static uint32_t resolution_y;
+
+int dpi_gather_metrics(void)
 {
 
+#if defined _WIN32
+
+    resolution_x = GetSystemMetrics(SM_CXSCREEN);
+    resolution_y = GetSystemMetrics(SM_CYSCREEN);
+
+#elif defined __linux__
+
+    Display* disp = XOpenDisplay(NULL);
+    if (!disp)
+    {
+        return 1;
+    }
+
+    Screen* scrn = DefaultScreenOfDisplay(disp);
+    if (!scrn)
+    {
+        return 2;
+    }
+
+    resolution_x = scrn->width;
+    resolution_y = scrn->height;
+
+#endif
+    
+    scale = (double)resolution_y / REFERENCE_PIXEL_HEIGHT;
+    if (scale < 1.0)
+    {
+        scale = 1.0;
+    }
+
+    return 0;
 }
 
 double dpi_get_monitor_scale(void)
 {
     return scale;
+}
+
+uint32_t dpi_get_resolution_x(void)
+{
+    return resolution_x;
+}
+
+uint32_t dpi_get_resolution_y(void)
+{
+    return resolution_y;
 }
 
